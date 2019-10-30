67facddf5697e8ed562a0fa10d6e7daaad698e92
BjornLuG <bjornlu.dev@gmail.com>
2019-10-30 15:46:46 +0800

    Remove store modules

--- src/store.js
+++ src/store.js
@@ -1,17 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import customers from './store/customers'
-import staffs from './store/staffs'
-import jobs from './store/jobs'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  modules: {
-    customers,
-    staffs,
-    jobs
-  },
   state: {
 
   },
