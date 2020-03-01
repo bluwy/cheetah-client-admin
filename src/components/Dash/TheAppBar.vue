6a79b41e107c7ca5d6eb6ba63cf671d93bbdb2bd
BjornLuG <bjornlu.dev@gmail.com>
2020-03-01 21:56:49 +0800

    Extract dash app bar to component

---
+++ src/components/Dash/TheAppBar.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-app-bar
+    app
+    elevate-on-scroll
+  >
+    <v-toolbar-title>
+      <v-btn
+        text
+        to="/"
+      >
+        E-JOB
+      </v-btn>
+    </v-toolbar-title>
+    <v-spacer />
+    <v-menu offset-y>
+      <template #activator="{ on }">
+        <v-btn
+          large
+          text
+          color="primary"
+          v-on="on"
+        >
+          <v-icon
+            large
+            :left="!!admin.username"
+          >
+            mdi-account-circle
+          </v-icon>
+          <span
+            v-if="admin.username"
+            class="ml-3"
+          >{{ admin.username }}</span>
+        </v-btn>
+      </template>
+      <v-list dense>
+        <v-list-item
+          v-if="isPrivilegeFull"
+          color="primary"
+          to="/manage"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-shield</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Manage</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item
+          color="error"
+          @click="logout()"
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-logout</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Log out</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
+</template>
+
+<script>
+import { get } from 'lodash-es'
+import ADMIN_GET_ONE from '@/graphql/Admin/GetOne.graphql'
+import AUTH_LOGOUT from '@/graphql/Auth/Logout.graphql'
+
+export default {
+  name: 'DashTheAppBar',
+  apollo: {
+    admin: {
+      // Get session admin
+      query: ADMIN_GET_ONE
+    }
+  },
+  data: () => ({
+    admin: {}
+  }),
+  computed: {
+    isPrivilegeFull () {
+      return get(this.admin, 'privilege') === 'FULL'
+    }
+  },
+  methods: {
+    async logout () {
+      try {
+        const { data: { logoutAdmin } } = await this.$apollo.mutate({
+          mutation: AUTH_LOGOUT
+        })
+
+        if (!logoutAdmin) {
+          throw new Error('Unable to logout')
+        }
+      } catch (e) {
+        // If logout failed, just log to console, no need to prevent client.
+        // This is because the session if not logout will also expire anyway.
+        console.error(e)
+      }
+
+      this.$router.push({ path: '/login' })
+    }
+  }
+}
+</script>
--- src/layouts/Dash.vue
+++ src/layouts/Dash.vue
@@ -1,66 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      elevate-on-scroll
-    >
-      <v-toolbar-title>
-        <v-btn
-          text
-          to="/"
-        >
-          E-JOB
-        </v-btn>
-      </v-toolbar-title>
-      <v-spacer />
-      <v-menu offset-y>
-        <template #activator="{ on }">
-          <v-btn
-            large
-            text
-            color="primary"
-            v-on="on"
-          >
-            <v-icon
-              large
-              :left="!!admin.username"
-            >
-              mdi-account-circle
-            </v-icon>
-            <span
-              v-if="admin.username"
-              class="ml-3"
-            >{{ admin.username }}</span>
-          </v-btn>
-        </template>
-        <v-list dense>
-          <v-list-item
-            v-if="isPrivilegeFull"
-            color="primary"
-            to="/manage"
-            link
-          >
-            <v-list-item-icon>
-              <v-icon>mdi-shield</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>Manage</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item
-            color="error"
-            @click="logout()"
-          >
-            <v-list-item-icon>
-              <v-icon>mdi-logout</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>Log out</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-app-bar>
+    <the-app-bar />
     <v-content>
       <transition mode="out-in">
         <router-view />
@@ -71,48 +11,14 @@
 </template>
 
 <script>
-import { get } from 'lodash-es'
+import TheAppBar from '@/components/Dash/TheAppBar.vue'
 import SnackbarGlobal from '@/components/Common/SnackbarGlobal.vue'
-import ADMIN_GET_ONE from '@/graphql/Admin/GetOne.graphql'
-import AUTH_LOGOUT from '@/graphql/Auth/Logout.graphql'
 
 export default {
   name: 'Dash',
-  apollo: {
-    admin: {
-      // Get session admin
-      query: ADMIN_GET_ONE
-    }
-  },
   components: {
+    TheAppBar,
     SnackbarGlobal
-  },
-  data: () => ({
-    admin: {}
-  }),
-  computed: {
-    isPrivilegeFull () {
-      return get(this.admin, 'privilege') === 'FULL'
-    }
-  },
-  methods: {
-    async logout () {
-      try {
-        const { data: { logoutAdmin } } = await this.$apollo.mutate({
-          mutation: AUTH_LOGOUT
-        })
-
-        if (!logoutAdmin) {
-          throw new Error('Unable to logout')
-        }
-      } catch (e) {
-        // If logout failed, just log to console, no need to prevent client.
-        // This is because the session if not logout will also expire anyway.
-        console.error(e)
-      }
-
-      this.$router.push({ path: '/login' })
-    }
   }
 }
 </script>
