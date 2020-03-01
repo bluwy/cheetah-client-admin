5b57fa2492d99f903f8c00e9676b20293b93e00d
BjornLuG <bjornlu.dev@gmail.com>
2020-03-01 16:41:00 +0800

    Remove local storage auth token

--- src/plugins/apollo.js
+++ src/plugins/apollo.js
@@ -1,19 +1,11 @@
 import Vue from 'vue'
 import VueApollo from 'vue-apollo'
 import ApolloClient from 'apollo-boost'
-import { authToken } from '@/utils/localStorage'
 
 Vue.use(VueApollo)
 
 export const apolloClient = new ApolloClient({
-  uri: process.env.VUE_APP_SERVER_URL,
-  request: (operation) => {
-    operation.setContext({
-      headers: {
-        authorization: authToken()
-      }
-    })
-  }
+  uri: process.env.VUE_APP_SERVER_URL
 })
 
 export default new VueApollo({
