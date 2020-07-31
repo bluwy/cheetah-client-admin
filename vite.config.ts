c6bb80b88a63aa308636a6094c153973a879f809
bluwy <bjornlu.dev@gmail.com>
2020-07-31 17:22:06 +0800

    Scaffold project

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import type { UserConfig } from 'vite'
+import reactPlugin from 'vite-plugin-react'
+
+const config: UserConfig = {
+  jsx: 'react',
+  plugins: [reactPlugin]
+}
+
+export default config
