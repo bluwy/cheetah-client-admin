5375be60cd951fca251cb57f9967e5570b7e0fb7
bluwy <bjornlu.dev@gmail.com>
2020-08-02 14:35:53 +0800

    Setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <div>Hello World</div>
-      <Button variant="contained" color="primary">Oh hi</Button>
-    </>
+    <main>
+      <Switch>
+        <Route path="/login">
+          Login
+        </Route>
+        <Route path="/reset-password">
+          Reset Password
+        </Route>
+        <Route path="/sudo">
+          Sudo
+        </Route>
+        <Route path="/dashboard">
+          Dashboard
+        </Route>
+        <Route>404</Route>
+      </Switch>
+    </main>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { CssBaseline } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('app'),
 );
