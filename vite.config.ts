86753cd96bb864a79c08d78b9b957393052a1ccb
bluwy <bjornlu.dev@gmail.com>
2020-08-02 12:14:54 +0800

    Setup src path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
+import path from 'path';
 import type { UserConfig } from 'vite';
 import reactPlugin from 'vite-plugin-react';
 
 const config: UserConfig = {
   jsx: 'react',
+  alias: {
+    '/@/': path.resolve(__dirname, 'src'),
+  },
   plugins: [reactPlugin],
 };
 
