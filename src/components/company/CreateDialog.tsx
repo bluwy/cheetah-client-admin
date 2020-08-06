7ccb3e33083cae7f0f891b773ae2e3731cb6dae5
bluwy <bjornlu.dev@gmail.com>
2020-08-06 22:21:40 +0800

    Finish company create dialog

--- src/components/company/CreateDialog.tsx
+++ src/components/company/CreateDialog.tsx
@@ -5,7 +5,7 @@ import {
   CompanyCreateDialogCompanyCreateMutation as CreateM,
   CompanyCreateDialogCompanyCreateMutationVariables as CreateV,
 } from '/@/schema';
-import { TextField } from '@material-ui/core';
+import { makeStyles, Box, TextField } from '@material-ui/core';
 import FormDialog, { FormDialogProps } from '/@/components/FormDialog';
 
 type CompanyCreateDialogProps = Omit<FormDialogProps, 'dialogTitle' | 'onSubmit'>;
@@ -23,12 +23,26 @@ const CREATE_COMPANY = gql`
   }
 `;
 
-function CompanyCreateDialog(props: CompanyCreateDialogProps) {
-  const { onClose, ...restProps } = props;
+const useStyles = makeStyles({
+  root: {
+    width: 300,
+  },
+});
 
-  const { register, handleSubmit, reset } = useForm<FormInput>();
+function CompanyCreateDialog(props: CompanyCreateDialogProps) {
+  const { onClose, classes: propClasses, ...restProps } = props;
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    errors,
+    formState,
+  } = useForm<FormInput>();
 
   const [createCompany] = useMutation<CreateM, CreateV>(CREATE_COMPANY);
+
+  const classes = useStyles();
 
   const handleClose = () => {
     reset();
@@ -50,23 +64,38 @@ function CompanyCreateDialog(props: CompanyCreateDialogProps) {
   return (
     <FormDialog
       {...restProps}
+      disableBackdropClick={formState.isDirty}
+      classes={{ ...propClasses, paper: classes.root }}
       dialogTitle="Create new company"
       onSubmit={handleSubmit(onSubmit)}
       onClose={handleClose}
     >
-      <TextField
-        name="name"
-        label="Name"
-        variant="outlined"
-        fullWidth
-        inputRef={register({ required: true })}
-      />
+      <Box mb={2}>
+        <TextField
+          name="name"
+          label="Name"
+          variant="outlined"
+          fullWidth
+          autoFocus
+          inputRef={register({
+            required: { value: true, message: 'Name is required' },
+          })}
+          error={!!errors.name}
+          helperText={errors.name?.message}
+        />
+
+      </Box>
       <TextField
         name="alias"
         label="Alias"
         variant="outlined"
         fullWidth
-        inputRef={register({ required: true, minLength: 1, maxLength: 1 })}
+        inputRef={register({
+          required: { value: true, message: 'Alias is required' },
+          pattern: { value: /^[A-Za-z]$/, message: 'Alias should only be a single alphabet' },
+        })}
+        error={!!errors.alias}
+        helperText={errors.alias?.message || 'Enter a single alphabet as the alias'}
       />
     </FormDialog>
   );
