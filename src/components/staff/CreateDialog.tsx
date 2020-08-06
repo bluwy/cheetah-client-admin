9fbe0d63d84fe1f3199ce57c4f1217a86b808080
bluwy <bjornlu.dev@gmail.com>
2020-08-06 21:43:38 +0800

    Finish staff create dialog

--- src/components/staff/CreateDialog.tsx
+++ src/components/staff/CreateDialog.tsx
@@ -5,7 +5,7 @@ import {
   StaffCreateDialogStaffCreateMutation as CreateM,
   StaffCreateDialogStaffCreateMutationVariables as CreateV,
 } from '/@/schema';
-import { TextField } from '@material-ui/core';
+import { makeStyles, Box, TextField } from '@material-ui/core';
 import FormDialog, { FormDialogProps } from '/@/components/FormDialog';
 
 type StaffCreateDialogProps = Omit<FormDialogProps, 'dialogTitle' | 'onSubmit'>;
@@ -23,12 +23,26 @@ const CREATE_STAFF = gql`
   }
 `;
 
-function StaffCreateDialog(props: StaffCreateDialogProps) {
-  const { onClose, ...restProps } = props;
+const useStyles = makeStyles({
+  root: {
+    width: 300,
+  },
+});
 
-  const { register, handleSubmit, reset } = useForm<FormInput>();
+function StaffCreateDialog(props: StaffCreateDialogProps) {
+  const { onClose, classes: propClasses, ...restProps } = props;
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    errors,
+    formState,
+  } = useForm<FormInput>();
 
   const [createStaff] = useMutation<CreateM, CreateV>(CREATE_STAFF);
+
+  const classes = useStyles();
 
   const handleClose = () => {
     reset();
@@ -50,23 +64,36 @@ function StaffCreateDialog(props: StaffCreateDialogProps) {
   return (
     <FormDialog
       {...restProps}
+      disableBackdropClick={formState.isDirty}
+      classes={{ ...propClasses, paper: classes.root }}
       dialogTitle="Create new staff"
       onSubmit={handleSubmit(onSubmit)}
       onClose={handleClose}
     >
-      <TextField
-        name="username"
-        label="Username"
-        variant="outlined"
-        fullWidth
-        inputRef={register({ required: true })}
-      />
+      <Box mb={2}>
+        <TextField
+          name="username"
+          label="Username"
+          variant="outlined"
+          fullWidth
+          autoFocus
+          inputRef={register({
+            required: { value: true, message: 'Username is required' },
+          })}
+          error={!!errors.username}
+          helperText={errors.username?.message}
+        />
+      </Box>
       <TextField
         name="fullName"
         label="Full name"
         variant="outlined"
         fullWidth
-        inputRef={register({ required: true })}
+        inputRef={register({
+          required: { value: true, message: 'Full name is required' },
+        })}
+        error={!!errors.fullName}
+        helperText={errors.fullName?.message}
       />
     </FormDialog>
   );
