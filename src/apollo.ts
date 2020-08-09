e8517f066b3308090bf5035256a3576ae73bb52d
bluwy <bjornlu.dev@gmail.com>
2020-08-09 22:37:41 +0800

    Add apollo client fetch credentials

--- src/apollo.ts
+++ src/apollo.ts
@@ -2,7 +2,8 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 import typePolicies from './store/policies';
 
 const client = new ApolloClient({
-  uri: import.meta.env.VITE_BACKEND_URL,
+  uri: import.meta.env.VITE_SERVER_URL,
+  credentials: 'include',
   cache: new InMemoryCache({
     typePolicies,
   }),
